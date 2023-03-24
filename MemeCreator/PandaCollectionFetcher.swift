9515fb347afd8b4cad4be1f85072578fd2ad48d3
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-24 09:20:07 +0600

    fetching panda data

--- MemeCreator/PandaCollectionFetcher.swift
+++ MemeCreator/PandaCollectionFetcher.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class PandaCollectionFetcher: ObservableObject {
+    @Published var imageData = PandaCollection(sample: [Panda.defaultPanda])
+    @Published var currentPanda = Panda.defaultPanda
     
+    let urlString = "http://playgrounds-cdn.apple.com/assets/pandaData.json"
+    
+    enum FetchError: Error {
+        case badRequest
+        case badJson
+    }
+    
+    func fetchData() async
+    throws {
+        guard let url = URL(string: urlString) else { return }
+        
+        let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url))
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {throw FetchError.badRequest}
+        
+        Task {@MainActor in
+            imageData = try JSONDecoder().decode(PandaCollection.self, from: data)
+        }
+    }
 }
