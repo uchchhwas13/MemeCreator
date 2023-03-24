87301a6cf7e65dbd65baccb34b88007d6d2cd568
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-24 08:53:11 +0600

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by Uchchhwas Roy on 24/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
